b962a3a609bab0d5185451eddcffff52be17f628
KAKA12-19 <evenezertadesse@gmail.com>
2025-06-28 22:23:52 +0300

    last change

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosBase = axios.create({
   //baseURL: "http://localhost:8000/api",
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
+  
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
